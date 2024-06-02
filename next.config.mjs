6d0efad55dc1c548860d5d6eceab1c0bcb19fc5a
Kirilanshelo <38433090+Kirilanshelo@users.noreply.github.com>
2024-06-02 18:21:43 +0200

    third commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/my-honeymoon-gallery",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+};
 
 export default nextConfig;
